7d7968757798b02c13f7d0ba72051a0deba69d65
Kristian Mansfield <kristianmansfield@protonmail.com>
2025-08-12 12:53:48 +1000

    Initial commit

---
+++ map_prereqs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Imports are always put at the top of the file, just after any module
+# comments and docstrings, and before module globals and constants.
+# Imports should be grouped in the following order:
+#   Standard library imports.
+#   Related third party imports.
+#   Local application/library specific imports.
